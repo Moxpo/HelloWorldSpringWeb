adf9b0eba7a0ea2e157808034554d4cdf5e8858e
Gregor <gregor.fiddes@gmail.com>
2023-04-12 18:21:23 +0100

    first commit

---
+++ src/test/java/com/example/helloworldspringweb/HelloWorldSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
